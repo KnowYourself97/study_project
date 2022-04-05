ccd4b4abb56b124db26da225cb554804395e362f
Dmitrii <7thespecialone49@gmail.com>
2022-04-05 21:13:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// let arr = ["plum.png", "orange.jpeg"];
+// console.log(arr[0]);
+// alert('Hello!');
+
+// const result = confirm('Are you here?');
+// console.log(typeof(result));
+
+// const answers = [];
+
+// // answers[0] = prompt('Name', '');
+// // answers[1] = prompt('Sex', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https//someurl.com/${category});
+
+// let incr = 10,
+//     decr = 10;
+// ++incr;
+// --decr;
+ 
+// console.log(incr);
+// console.log(decr);
+
