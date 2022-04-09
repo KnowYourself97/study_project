f2b5a84d3a44a927757b847a4b1cbc84471b8851
Dmitrii <7thespecialone49@gmail.com>
2022-04-09 21:57:45 +0300

    thirdPractice

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const numberOfFilms = +prompt('How many films you watched?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('How many films you watched?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many films you watched?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,28 +17,53 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('one of the last films?', ''),
-        b = prompt('you score from 0 to 10?', '');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Done!');
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('one of the last films?', ''),
+            b = prompt('you score from 0 to 10?', '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done!');
+        } else {
+            console.log('Error');
+            i--;
+        }
+    }
+}
+
+rememberMyFilms();
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('You watched not enough films!');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('You are classic!');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('You are cinemaholic!');
     } else {
-        console.log('Error');
-        i--;
+        console.log('Error!');
     }
 
 }
 
-if (personalMovieDB.count < 10) {
-    console.log('You watched not enough films!');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <30) {
-    console.log('You are classic!');
-} else if (personalMovieDB.count >= 30) {
-    console.log('You are cinemaholic!');
-} else {
-    console.log('Error!');
+detectPersonalLevel();
+
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
 }
 
-console.log(personalMovieDB);
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Your favorite movie is number ${i}`);
+    }
+}
+writeYourGenres();
