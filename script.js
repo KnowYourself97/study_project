bb988f2b6eb99f3f024d33d1b4d839fe176f41d2
Dmitrii <7thespecialone49@gmail.com>
2022-04-10 13:03:30 +0300

    callbackExample

--- script.js
+++ script.js
@@ -67,3 +67,24 @@ function writeYourGenres() {
     }
 }
 writeYourGenres();
+
+// function first() {    //callback example
+//     setTimeout(function() {
+//         console.log(1);
+//     }, 500);
+// }
+
+// function second() {
+//     console.log(2);
+// }
+//  first();
+//  second();
+
+//  function learnJS(lang, callback) {
+//      console.log(`I am learning: ${lang}`);
+//      callback();
+//  }
+
+//  learnJS('JavaScript', function() {
+//      console.log('This lesson done!');
+//  }); 
