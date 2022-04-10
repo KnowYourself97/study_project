0b41f7fc59b94142f7758892fd0662093a1982e0
Dmitrii <7thespecialone49@gmail.com>
2022-04-10 13:38:05 +0300

    objects,destructorization,property

--- script.js
+++ script.js
@@ -88,3 +88,47 @@ writeYourGenres();
 //  learnJS('JavaScript', function() {
 //      console.log('This lesson done!');
 //  }); 
+
+// objects, destructorization, property
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function() {
+//         console.log('Test');
+//     }
+// };
+
+// options.makeTest();
+// console.log(Object.keys(options).length);
+
+// const {border, bg} = options.colors; // destructorization
+// console.log(bg, border);
+
+//console.log(options.colors.border);
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof (options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Setting ${i}, has value ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Setting ${key}, has value ${options[key]}`);
+//         counter++;
+//     }
+// }
+
+// console.log(counter);
+
